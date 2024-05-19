364a7979330c5ffed5b234c387f6737c727a7532
Valentina Nikolaeva <61709121+Valley777@users.noreply.github.com>
2024-05-19 15:09:07 +0300

    Add files via upload

---
+++ messageEmulating.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+public class messageEmulating {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        int counter = 0;
+           int sequence = Integer.parseInt(scan.nextLine());
+           while(counter<=n){
+               if(sequence==2){
+                   System.out.print("a");}
+               if(sequence==22){
+                   System.out.print("b");}
+               if(sequence==222){
+                   System.out.print("c");}
+               if(sequence==3){
+                   System.out.print("d");}
+               if(sequence==33){
+                   System.out.print("e");}
+               if(sequence==333){
+                   System.out.print("f");}
+               if(sequence==4){
+                   System.out.print("g");}
+               if(sequence==44){
+                   System.out.print("h");}
+               if(sequence==444){
+                   System.out.print("i");}
+               if(sequence==5){
+                   System.out.print("j");}
+               if(sequence==55){
+                   System.out.print("k");}
+               if(sequence==555){
+                   System.out.print("l");}
+               if(sequence==6){
+                   System.out.print("m");}
+               if(sequence==66){
+                   System.out.print("n");}
+               if(sequence==666){
+                   System.out.print("o");}
+               if(sequence==7){
+                   System.out.print("p");}
+               if(sequence==77){
+                   System.out.print("q");}
+               if(sequence==777){
+                   System.out.print("r");}
+               if(sequence==7777){
+                   System.out.print("s");}
+               if(sequence==8){
+                   System.out.print("t");}
+               if(sequence==88){
+                   System.out.print("u");}
+               if(sequence==888){
+                   System.out.print("v");}
+               if(sequence==9){
+                   System.out.print("w");}
+               if(sequence==99){
+                   System.out.print("x");}
+               if(sequence==999){
+                   System.out.print("y");}
+               if(sequence==9999){
+                   System.out.print("z");}
+               if(sequence==0){
+                   System.out.print(" ");}
+             sequence = Integer.parseInt(scan.nextLine());
+           }
+           }}
+
+
